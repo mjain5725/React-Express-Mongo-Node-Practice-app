75a7cda250c4502e8f45ed4329f5b37d97dd95fe
Manish Jain <52937411+mjain5725@users.noreply.github.com>
2020-10-03 21:21:44 -0400

    Added redux and alert component

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,29 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
 import Register from './components/auth/Register';
+//REDUX
+import { Provider } from 'react-redux';
+import store from './store';
 import './App.css';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <NavBar />
-      <Route exact path='/' component={Landing} />
-      <section className='container'>
-        <Switch>
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <NavBar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Alert />
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
