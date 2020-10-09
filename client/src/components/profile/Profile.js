bee58d35641c79dc7bbf40583122a3fb7f275bbb
Manish Jain <52937411+mjain5725@users.noreply.github.com>
2020-10-09 12:08:22 -0400

    Updated

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,7 +5,9 @@ import { getProfileById } from '../../store/actions/index';
 import PropTypes from 'prop-types';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
 import { Link } from 'react-router-dom';
+import ProfileEducation from './ProfileEducation';
 
 const Profile = ({
   getProfileById,
@@ -37,6 +39,36 @@ const Profile = ({
           <div className='profile-grid my-1'>
             <ProfileTop profile={profile} />
             <ProfileAbout profile={profile} />
+            <div className='profile-exp bg-white p-2'>
+              <h2 className='text-primary'>Experience</h2>
+              {profile.experience.length > 0 ? (
+                <Fragment>
+                  {profile.experience.map(experience => (
+                    <ProfileExperience
+                      key={experience._id}
+                      experience={experience}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No experience credentials</h4>
+              )}
+            </div>
+            <div className='profile-edu bg-white p-2'>
+              <h2 className='text-primary'>Education</h2>
+              {profile.education.length > 0 ? (
+                <Fragment>
+                  {profile.education.map(education => (
+                    <ProfileEducation
+                      key={education._id}
+                      education={education}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No education credentials</h4>
+              )}
+            </div>
           </div>
         </Fragment>
       )}
